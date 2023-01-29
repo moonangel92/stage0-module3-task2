9cf26a541a13b67b1ad9bb0796fc0ee1def5e9e6
aiperi <aiperi.damirkyzy@gmail.com>
2023-01-30 02:43:42 +0600

    third commit

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -6,8 +6,8 @@ public class NamingConvention {
         int variable_One = 1;
         int variableTwo = 2;
         int variable_three = 3;
-        final int VariableFour = 4;
-        final int VariableFive = 5;
+        final int VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE = 5;
         int VariableSix = 6;
         int variableSeven = 7;
         int VariableEight = 8;
@@ -16,8 +16,8 @@ public class NamingConvention {
         System.out.println(variable_One);
         System.out.println(variableTwo);
         System.out.println(variable_three);
-        System.out.println(VariableFour);
-        System.out.println(VariableFive);
+        System.out.println(VARIABLE_FOUR);
+        System.out.println(VARIABLE_FIVE);
         System.out.println(VariableSix);
         System.out.println(variableSeven);
         System.out.println(VariableEight);
